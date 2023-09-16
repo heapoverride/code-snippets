6688f5649bfa236207f695aab4ea1f96a867d1b7
UnrealSecurity <arran.bishop89@aol.com>
2023-09-16 17:33:55 +0300

    Changed indentation to spaces

--- Methods/HashCode.cs
+++ Methods/HashCode.cs
@@ -5,16 +5,16 @@
 /// <returns>Hash code</returns>
 public static ulong HashCode(string str)
 {
-	ulong hash = 3074457345618258791ul;
+    ulong hash = 3074457345618258791ul;
 
-	unchecked
-	{
-		for (int i = 0; i < str.Length; i++)
-		{
-			hash += str[i];
-			hash *= 3074457345618258799ul;
-		}
-	}
+    unchecked
+    {
+        for (int i = 0; i < str.Length; i++)
+        {
+            hash += str[i];
+            hash *= 3074457345618258799ul;
+        }
+    }
 
-	return hash;
+    return hash;
 }
