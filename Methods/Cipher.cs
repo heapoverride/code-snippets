30ed5b391267a4ef9c9c096a6a2a30dc5499112e
UnrealSecurity <arran.bishop89@aol.com>
2023-09-18 17:32:31 +0300

    Added Cipher.cs

---
+++ Methods/Cipher.cs
@@ -0,0 +1,12 @@
+/// <summary>
+/// Simple XOR cipher
+/// </summary>
+/// <param name="data"></param>
+/// <param name="key"></param>
+public static Cipher(byte[] data, byte[] key)
+{
+    for (int i = 0; i < data.Length; i++)
+    {
+        data[i] ^= key[i % key.Length];
+    }
+}
