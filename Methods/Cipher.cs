c13168000db1b843f91b6bc87c6f40f92f211323
UnrealSecurity <arran.bishop89@aol.com>
2023-09-18 17:33:44 +0300

    Fixed: Forgot to add the return type (void)

--- Methods/Cipher.cs
+++ Methods/Cipher.cs
@@ -3,7 +3,7 @@
 /// </summary>
 /// <param name="data"></param>
 /// <param name="key"></param>
-public static Cipher(byte[] data, byte[] key)
+public static void Cipher(byte[] data, byte[] key)
 {
     for (int i = 0; i < data.Length; i++)
     {
